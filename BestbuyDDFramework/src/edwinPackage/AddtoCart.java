84882df260b953b7605905324ae4b0743b485136
jino123 <edwind.2008@gmail.com>
2017-06-19 15:28:22 +0530

    edwin package is added Add to cart is added.

---
+++ BestbuyDDFramework/src/edwinPackage/AddtoCart.java
@@ -0,0 +1,13 @@
+package edwinPackage;
+
+import org.testng.annotations.Test;
+
+public class AddtoCart {
+	
+	@Test
+	public void addtoCart()
+	{
+		System.out.println("****Add to cart***********");
+	}
+
+}
