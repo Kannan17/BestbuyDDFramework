e0134aa5d05912d8f575418e71798b0597f6c417
Umadevi <Uma.Raj0402@gmail.com>
2017-07-04 14:51:58 +0530

    Registered User login Test Case

---
+++ BestbuyDDFramework/src/umaPackage/Registereduser.java
@@ -0,0 +1,91 @@
+package umaPackage;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import reports.ExtentR;
+import utilPakage.ExcelData;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+import basePackage.Base;
+
+public class Registereduser extends Base {
+	
+	@Test(dataProvider="getData")
+	public void tc4(String s,String s1) throws IOException
+	{
+		
+		
+		// Call the getInstance() method to get the Extent Report object
+		rep=ExtentR.getInstance();
+		// Start the test and assign that to Extent test reference variable "test"
+		test=rep.startTest("Registereduser");
+		// log the desired info
+		test.log(LogStatus.INFO, "Signin test started");
+		System.out.println("tc4() start");
+		
+		
+		//initiating the browser
+		init();
+		//get the URL
+		
+		naviagate();
+		// Select the language
+		select("languageSelection_name","language");
+	
+		//Select Go button and Store 
+		click("goButton_xpath");
+		
+		//Select the store locator
+		click("storeLocator_xpath");
+		
+		//login in as Registered user 
+		click("account_id");
+		click("signIn_xpath");
+		type("emailAddress_id",s);
+		type("loginPassword_id",s1);
+		click("signInbutton_xpath");
+	
+		
+		//String yourtext= driver.findElement(By.tagName("body")).getText() ;
+		
+		//System.out.println("");
+	}
+	
+	
+	@AfterTest
+	public void end1()
+	{
+		// To generate the report write the below two codes 
+	rep.endTest(test);
+	rep.flush();
+	
+	// close the browser
+	//driver.quit();
+	}
+	
+	
+	@DataProvider
+	public String[][] getData() throws EncryptedDocumentException, InvalidFormatException, IOException
+	{
+	
+		
+		String data[][];
+		
+		data= ExcelData.getExcelData("Registereduser");
+		
+		return data;
+	}		
+
+}
