c9dd65bcb5915f32bd264530baa47aaa603154a0
kannan17 <kannan0302@gmail.com>
2017-07-09 07:19:18 +0530

    fs text is removed

--- BestbuyDDFramework/src/utilPakage/ExcelData.java
+++ BestbuyDDFramework/src/utilPakage/ExcelData.java
@@ -19,7 +19,7 @@ public class ExcelData {
 	
 	{
 		
-		// Get the file to the reference variable fs
+		// Get the file to the reference variable
 		FileInputStream fs = new FileInputStream(System.getProperty("user.dir")+"\\data.xlsx");
 		//Create a workbook in POI with fs data
 	    Workbook wb =  WorkbookFactory.create(fs);
